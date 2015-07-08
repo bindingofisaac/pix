0daef2be593b398c9c60ce6ad7142f14b9ad869d
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-08 21:57:06 +0530

    Utility class

---
+++ include/Utils.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+
+std::string read_file(const std::string);
---
+++ src/Utils.cpp
@@ -0,0 +1,12 @@
+#include <Utils.hpp>
+
+std::string read_file(const std::string &filePath){
+    std::ifstream file(filePath);
+    // TODO: check for error
+    std::string fileContents = "", line;
+    while(std::getline(file, line)){
+        fileContents += line + "\n";
+    }
+    file.close();
+    return fileContents;
+}
