dc89ce6751ee823dbfff57810aba772353e26402
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-11 02:15:17 +0530

    per vertex color

--- main.cpp
+++ main.cpp
@@ -18,12 +18,22 @@ int main(){
         -0.5f, -0.5f
     };
 
+    float color[] = {
+        1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
     GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
 
     Pix::VertexArray  vao;
     Pix::VertexBuffer *vbo = new Pix::VertexBuffer(vertices, 8, 2);
+    Pix::VertexBuffer *col = new Pix::VertexBuffer(color, 12, 3);
     Pix::IndexBuffer  ibo(indices, 6);
+
     vao.add(vbo, 0);
+    vao.add(col, 1);
 
     vao.bind();
     ibo.bind();
