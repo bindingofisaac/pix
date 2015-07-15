892729955132db4dbc04b71a60a68e5d4b61feae
bindingofisaac <bindingofisaacs@gmail.com>
2015-07-15 23:17:25 +0530

    simplified renderable2d

--- include/Renderable2D.hpp
+++ include/Renderable2D.hpp
@@ -16,40 +16,12 @@ namespace Pix{
             glm::vec2 m_Size;
             glm::vec4 m_Color;
 
-            VertexArray  *m_VertexArray;
-            IndexBuffer  *m_IndexBuffer;
         public:
             Renderable2D(glm::vec3 position, glm::vec2 size, glm::vec4 color) : m_Position(position), m_Size(size), m_Color(color) {
-                m_VertexArray = new VertexArray();
-                GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
-                m_IndexBuffer = new IndexBuffer(indices, 6);
-
-                GLfloat vertices[] = {
-                    position.x          , position.y          , position.z ,
-                    position.x          , position.y + size.y , position.z ,
-                    position.x + size.x , position.y + size.y , position.z ,
-                    position.x + size.x , position.y          , position.z
-                };
-
-                GLfloat col[] = {
-                    color.r, color.g, color.b, color.a,
-                    color.r, color.g, color.b, color.a,
-                    color.r, color.g, color.b, color.a,
-                    color.r, color.g, color.b, color.a
-                };
-
-                m_VertexArray->add(new VertexBuffer(vertices, 4*3, 3), 0);
-                m_VertexArray->add(new VertexBuffer(col, 4*4, 4), 1);
-
             }
 
             ~Renderable2D(){
-                delete m_VertexArray;
-                delete m_IndexBuffer;
             }
-
-            inline const VertexArray* getVAO() const { return m_VertexArray; }
-            inline const IndexBuffer* getIBO() const { return m_IndexBuffer; }
 
             inline const glm::vec3& getPosition() const { return m_Position; }
             inline const glm::vec4& getColor() const { return m_Color; }
